0dfdc6fcc49d39cb4f79fd8e97472250ad6134cc
neginasu <neginasu7@gmail.com>
2023-09-23 22:33:36 +0900

    fix: ハイドレーションエラーを回避するためLinkタグを移動

--- src/components/post/singlePost.tsx
+++ src/components/post/singlePost.tsx
@@ -12,23 +12,23 @@ export const SinglePost = (props: Props) => {
   const { title, description, date, tags, slug } = props;
   return (
     <>
-      <Link href={`/posts/${slug}`}>
-        <section className="lg:w-1/2 bg-violet-300 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
-          <div className="flex items-center gap-3">
-            <h2 className="text-gray-500">{title}</h2>
-            <div className="text-gray-500">{date}</div>
-            {tags.map((tag: string) => (
-              <span
-                className="text-white bg-violet-400 rounded-xl px-2 font-medium"
-                key={tag}
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
-          <p className="text-gray-500">{description}</p>
-        </section>
-      </Link>
+      <section className="lg:w-1/2 bg-violet-300 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
+        <div className="flex items-center gap-3">
+          <h2 className="text-gray-500">
+            <Link href={`/posts/${slug}`}>{title} </Link>
+          </h2>
+          <div className="text-gray-500">{date}</div>
+          {tags.map((tag: string) => (
+            <span
+              className="text-white bg-violet-400 rounded-xl px-2 font-medium"
+              key={tag}
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <p className="text-gray-500">{description}</p>
+      </section>
     </>
   );
 };
